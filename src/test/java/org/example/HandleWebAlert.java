f923296285fb9cefd25741c38febf3e1794a0b0a
MD ABDUR RAHMAN <45840084+arfoysal@users.noreply.github.com>
2024-12-06 22:29:07 +0600

    Add Select, and Alert

---
+++ src/test/java/org/example/HandleWebAlert.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class HandleWebAlert extends DriverSetup{
+    @Test
+    public void testAlert() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+
+        driver.findElement(By.xpath("//button[@onclick='jsAlert()']")).click();
+
+        Thread.sleep(2000);
+
+        Alert alert = driver.switchTo().alert();
+
+        System.out.println(alert.getText());
+
+        alert.accept();
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//button[@onclick='jsConfirm()']")).click();
+        alert = driver.switchTo().alert();
+        Thread.sleep(2000);
+        System.out.println(alert.getText());
+
+        alert.dismiss();
+
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//button[@onclick='jsPrompt()']")).click();
+        alert = driver.switchTo().alert();
+        Thread.sleep(2000);
+        System.out.println(alert.getText());
+
+        alert.sendKeys("Hello");
+
+        alert.accept();
+
+        Thread.sleep(2000);
+
+
+    }
+}
